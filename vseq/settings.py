47689e98a24090eb9e47bfd59761e8a4c72ee506
Jakob Drachmann Havtorn <jdh@corti.ai>
2021-03-09 15:12:50 +0000

    Add settings

---
+++ vseq/settings.py
@@ -0,0 +1,34 @@
+"""Settings file for infotropy package"""
+
+import os
+import logging
+
+import rootpath
+
+
+# Logging
+LOG_FORMAT = "%(asctime)-15s - %(module)-20s - %(levelname)-7s | %(message)s"
+LOG_LEVEL = os.getenv("VSEQ_LOG_LEVEL", "INFO")
+
+logging.basicConfig(format=LOG_FORMAT, level=logging.getLevelName(LOG_LEVEL))
+
+# Turn down log level of Trains
+logging.getLogger(name="trains.Task").setLevel(logging.WARNING)
+
+
+# Log all environment variables to Trains by default
+os.environ["TRAINS_LOG_ENVIRONMENT"] = os.getenv("TRAINS_LOG_ENVIRONMENT", "*")
+
+
+# Directories
+ROOT_PATH = rootpath.detect()
+
+TENSORBOARD_DIRECTORY = os.getenv("VSEQ_TENSORBOARD_DIRECTORY", os.path.join(ROOT_PATH, "tensorboard"))
+TRAINS_DIRECTORY = os.getenv("VSEQ_TRAINS_DIRECTORY", os.path.join(ROOT_PATH, "trains"))
+DATA_DIRECTORY = os.getenv("VSEQ_DATA_DIRECTORY", os.path.join(ROOT_PATH, "data"))
+
+TRAINS_DIRECTORY_NAS = os.getenv("VSEQ_TRAINS_DIRECTORY_NAS", os.path.join("/nas/experiments/trains"))
+
+
+for path in [TRAINS_DIRECTORY, DATA_DIRECTORY]:
+    os.makedirs(path, exist_ok=True)
