64f0e68e3ebf5538ce5b9b1659a6d013ca365da7
borgholt <lasseborgholt@gmail.com>
2021-04-13 16:05:56 +0000

    Added LSTMLM model and experiment script

---
+++ experiments/experiment_lstmlm.py
@@ -0,0 +1,141 @@
+import argparse
+import logging
+import random
+
+import torch
+import wandb
+import rich
+
+from torch.utils.data import DataLoader
+
+import vseq
+import vseq.data
+import vseq.models
+import vseq.utils
+import vseq.utils.device
+
+from vseq.data import DataModule, BaseDataset
+from vseq.data.batcher import TextBatcher
+from vseq.data.datapaths import PENN_TREEBANK_TRAIN, PENN_TREEBANK_VALID
+from vseq.data.tokens import DELIMITER_TOKEN
+from vseq.data.tokenizers import word_tokenizer
+from vseq.data.token_map import TokenMap
+from vseq.data.transforms import EncodeInteger
+from vseq.data.vocabulary import load_vocabulary
+from vseq.evaluation import Tracker
+from vseq.utils.rand import set_seed
+from vseq.utils.argparsing import str2bool
+from vseq.training import CosineAnnealer
+
+
+LOGGER = logging.getLogger(name=__file__)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--batch_size", default=32, type=int, help="batch size")
+parser.add_argument("--lr", default=3e-4, type=float, help="base learning rate")
+parser.add_argument("--embedding_dim", default=353, type=int, help="dimensionality of embedding space")
+parser.add_argument("--hidden_size", default=191, type=int, help="dimensionality of hidden state in LSTM")
+parser.add_argument("--word_dropout", default=0.38, type=float, help="word dropout probability")
+parser.add_argument("--epochs", default=500, type=int, help="number of epochs")
+parser.add_argument("--cache_dataset", default=True, type=str2bool, help="if True, cache the dataset in RAM")
+parser.add_argument("--num_workers", default=4, type=int, help="number of dataloader workers")
+# parser.add_argument("--seed", default='random', type=int, help="random seed")
+parser.add_argument("--device", default="auto", choices=["auto", "cuda", "cpu"])
+
+args, _ = parser.parse_known_args()
+
+# if args.seed == 'random':
+#     args.seed = random.randint(a=1e11, b=1e12)
+
+wandb.init(
+    entity="vseq",
+    project="vseq",
+    group="lstmlm",
+)
+wandb.config.update(args)
+rich.print(vars(args))
+
+# set_seed(args.seed)
+
+device = vseq.utils.device.get_device() if args.device == "auto" else torch.device(args.device)
+
+vocab = load_vocabulary(PENN_TREEBANK_TRAIN)
+token_map = TokenMap(tokens=vocab, add_start=False, add_end=False, add_delimit=True)
+penn_treebank_transform = EncodeInteger(
+    token_map=token_map,
+    tokenizer=word_tokenizer,
+)
+batcher = TextBatcher()
+
+modalities = [("txt", penn_treebank_transform, batcher)]
+
+train_dataset = BaseDataset(
+    source=PENN_TREEBANK_TRAIN,
+    modalities=modalities,
+    cache=args.cache_dataset,
+)
+val_dataset = BaseDataset(
+    source=PENN_TREEBANK_VALID,
+    modalities=modalities,
+    cache=args.cache_dataset,
+)
+
+train_loader = DataLoader(
+    dataset=train_dataset,
+    collate_fn=train_dataset.collate,
+    num_workers=args.num_workers,
+    shuffle=True,
+    batch_size=args.batch_size,
+)
+val_loader = DataLoader(
+    dataset=val_dataset,
+    collate_fn=val_dataset.collate,
+    num_workers=args.num_workers,
+    shuffle=False,
+    batch_size=args.batch_size,
+)
+
+delimiter_token_idx = token_map.get_index(DELIMITER_TOKEN)
+model = vseq.models.LSTMLM(
+    num_embeddings=len(token_map),
+    embedding_dim=args.embedding_dim,
+    hidden_size=args.hidden_size,
+    delimiter_token_idx=delimiter_token_idx
+)
+
+wandb.watch(model, log='all', log_freq=len(train_loader))
+
+model = model.to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+print(model)
+
+x, x_sl = next(iter(train_loader))[0]
+x = x.to(device)
+print(model.summary(input_example=x, x_sl=x_sl))
+
+tracker = Tracker()
+
+for epoch in tracker.epochs(args.epochs):
+
+    model.train()
+    for (x, x_sl), metadata in tracker(train_loader):
+        x = x.to(device)
+
+        loss, metrics, outputs = model(x, x_sl, word_dropout_rate=args.word_dropout)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        tracker.update(metrics)
+
+    model.eval()
+    for (x, x_sl), metadata in tracker(val_loader):
+        x = x.to(device)
+
+        loss, metrics, outputs = model(x, x_sl)
+
+        tracker.update(metrics)
+
+    # Log tracker metrics
+    tracker.log()
--- vseq/models/__init__.py
+++ vseq/models/__init__.py
@@ -1,4 +1,5 @@
 from vseq.models.get_model import get_model
 from vseq.models.bowman import Bowman
+from vseq.models.lstmlm import LSTMLM
 from vseq.models.wavenet.model import WaveNet
 from vseq.models.base_module import BaseModule
