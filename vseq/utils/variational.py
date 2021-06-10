9fd138bed275a3f2537619014f9465ac6c0da638
Jakob Drachmann Havtorn <jdh@corti.ai>
2021-06-10 09:47:22 +0000

    Rename argument

--- vseq/utils/variational.py
+++ vseq/utils/variational.py
@@ -50,7 +50,7 @@ def rsample_gaussian(mu, sd):
 
 def discount_free_nats(
     kl: TensorType["B":..., "shared":...],
-    free_bits: float = None,
+    free_nats: float = None,
     shared_dims: Union[Tuple[int], int] = None,
 ) -> torch.Tensor:
     """Free bits as introduced in [1] but renamed to free nats because that's what it really is with log_e.
@@ -58,7 +58,7 @@ def discount_free_nats(
     In the paper they divide all latents Z into K groups. This implementation assumes a that each KL tensor passed
     to __call__ is one such group.
 
-    The KL tensor may have more dimensions than the batch dimension, in which case the free_bits budget can be
+    The KL tensor may have more dimensions than the batch dimension, in which case the free_nats budget can be
     distributed across those dimensions by setting `shared_dims`. E.g. if `kl.shape` is (32, 10) and `shared_dims` is -1
     each of the 10 elements in the last dimension will get 1/10 of the free nats budget.
     If `kl.shape` is (32, 10, 10) and `shared_dims` is (-2, -1) each of the 10*10=100 elements will get 1 / 100th.
@@ -67,7 +67,7 @@ def discount_free_nats(
 
     [1] https://arxiv.org/pdf/1606.04934
     """
-    if free_bits == 0 or free_bits is None:
+    if free_nats == 0 or free_nats is None:
         return kl
 
     if isinstance(shared_dims, int):
@@ -75,7 +75,7 @@ def discount_free_nats(
 
     # equally divide free nats budget over the elements
     n_elements = math.prod(shared_dims) if shared_dims is not None else 1
-    min_kl_per_dim = free_bits / n_elements
+    min_kl_per_dim = free_nats / n_elements
 
     min_kl_per_dim = torch.tensor(min_kl_per_dim, dtype=kl.dtype, device=kl.device)
     freenats_kl = torch.maximum(kl, min_kl_per_dim)
