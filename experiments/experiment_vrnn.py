Fix free nats calculation

experiments/experiment_vrnn.py
@@ -1,5 +1,4 @@
 import argparse
-from vseq.utils.argparsing import str2bool
 
 import torch
 import wandb
@@ -23,8 +22,9 @@ from vseq.data.token_map import TokenMap
 from vseq.data.transforms import Compose, EncodeInteger, TextCleaner
 from vseq.data.vocabulary import load_vocabulary
 from vseq.evaluation import Tracker
-from vseq.utils.rand import set_seed, get_random_seed
 from vseq.training.annealers import CosineAnnealer
+from vseq.utils.argparsing import str2bool
+from vseq.utils.rand import set_seed, get_random_seed
 
 
 parser = argparse.ArgumentParser()
@@ -40,12 +40,14 @@ parser.add_argument("--dropout", default=0.0, type=float, help="dropout")
 parser.add_argument("--beta_anneal_steps", default=0, type=int, help="number of steps to anneal beta")
 parser.add_argument("--beta_start_value", default=0, type=float, help="initial beta annealing value")
 parser.add_argument("--free_nats_steps", default=0, type=int, help="number of steps to constant/anneal free bits")
-parser.add_argument("--free_nats_start_value", default=8, type=float, help="free bits per timestep")
+parser.add_argument("--free_nats_start_value", default=1, type=float, help="free bits per timestep")
 parser.add_argument("--token_level", default="word", type=str, choices=["word", "char"], help="word or character level")
-parser.add_argument("--epochs", default=250, type=int, help="number of epochs")
+parser.add_argument("--epochs", default=100, type=int, help="number of epochs")
 parser.add_argument("--num_workers", default=4, type=int, help="number of dataloader workers")
 parser.add_argument("--seed", default=None, type=int, help="random seed")
 parser.add_argument("--device", default="auto", choices=["auto", "cuda", "cpu"])
+parser.add_argument("--wandb_group", default=None, type=str, help="group for wandb")
+
 
 args, _ = parser.parse_known_args()
 
@@ -61,7 +63,7 @@ device = vseq.utils.device.get_device() if args.device == "auto" else torch.devi
 wandb.init(
     entity="vseq",
     project="vrnn",
-    group=None,
+    group=args.wandb_group,
 )
 wandb.config.update(args)
 rich.print(vars(args))
@@ -149,8 +151,8 @@ tracker = Tracker()
 
 beta_annealer = CosineAnnealer(anneal_steps=args.beta_anneal_steps, start_value=args.beta_start_value, end_value=1)
 free_nats_annealer = CosineAnnealer(
-    anneal_steps=args.free_nats_steps // 2,
-    constant_steps=args.free_nats_steps // 2,
+    anneal_steps=args.free_nats_steps // 4 * 3,
+    constant_steps=args.free_nats_steps // 4,
     start_value=args.free_nats_start_value,
     end_value=0,
 )

vseq/models/vrnn.py
@@ -211,17 +211,17 @@ class VRNN(nn.Module):
         free_nats: float = 0,
     ):
         """Return reduced loss for batch and non-reduced ELBO, log p(x|z) and KL-divergence"""
-        
+
         seq_mask = sequence_mask(x_sl, dtype=float, device=y.device)
-        
+
         log_prob_twise = self.likelihood.log_prob(y, logits) * seq_mask
         log_prob = log_prob_twise.view(y.size(0), -1).sum(1)  # (B,)
 
         kl = (kld_twise * seq_mask.unsqueeze(1)).sum((1, 2))  # (B,)
         elbo = log_prob - kl  # (B,)
 
-        kld_twise = discount_free_nats(kld_twise, free_nats, 1)
-        kl = (kld_twise * seq_mask.unsqueeze(1)).sum((1, 2))  # (B,)
+        kld_twise_fn = discount_free_nats(kld_twise, free_nats, shared_dims=1)
+        kl = (kld_twise_fn * seq_mask.unsqueeze(1)).sum((1, 2))  # (B,)
         loss = -(log_prob - beta * kl).sum() / x_sl.sum()  # (1,)
 
         return loss, elbo, log_prob, kl

vseq/utils/variational.py
@@ -58,24 +58,29 @@ def discount_free_nats(
     In the paper they divide all latents Z into K groups. This implementation assumes a that each KL tensor passed
     to __call__ is one such group.
 
-    The KL tensor may have more dimensions than the batch dimension, in which case the free_nats budget can be
-    distributed across those dimensions by setting `shared_dims`. E.g. if `kl.shape` is (32, 10) and `shared_dims` is -1
-    each of the 10 elements in the last dimension will get 1/10 of the free nats budget.
-    If `kl.shape` is (32, 10, 10) and `shared_dims` is (-2, -1) each of the 10*10=100 elements will get 1 / 100th.
+    By default, this method discounts `free_nats` units of nats elementwise in the KL regardless of its shape.
 
-    The returned free nats KL is equal to max(kl, freebits_per_dim, dim=shared_dims)
+    If the KL tensor has more dimensions than the batch dimension, the free_nats budget can be optionally
+    shared across those dimensions by setting `shared_dims`. E.g. if `kl.shape` is (32, 10) and `shared_dims` is -1,
+    each of the 10 elements in the last dimension will get 1/10th of the free nats budget. If `kl.shape` is (32, 10, 10)
+    and `shared_dims` is (-2, -1) each of the 10*10=100 elements will get 1 / 100th.
+
+    The returned KL with `free_nats` discounted is equal to max(kl, freebits_per_dim)
 
     [1] https://arxiv.org/pdf/1606.04934
     """
-    if free_nats == 0 or free_nats is None:
+    if free_nats is None or free_nats == 0:
         return kl
 
     if isinstance(shared_dims, int):
         shared_dims = (shared_dims,)
 
-    # equally divide free nats budget over the elements
-    n_elements = math.prod(shared_dims) if shared_dims is not None else 1
-    min_kl_per_dim = free_nats / n_elements
+    # equally divide free nats budget over the elements in shared_dims
+    if shared_dims is not None:
+        n_elements = math.prod([kl.shape[d] for d in shared_dims])
+        min_kl_per_dim = free_nats / n_elements
+    else:
+        min_kl_per_dim = free_nats
 
     min_kl_per_dim = torch.tensor(min_kl_per_dim, dtype=kl.dtype, device=kl.device)
     freenats_kl = torch.maximum(kl, min_kl_per_dim)