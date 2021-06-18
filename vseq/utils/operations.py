34a5fbcd7917342b7bb4d6f01852d04ce7452ef5
Jakob Drachmann Havtorn <jdh@corti.ai>
2021-06-18 13:24:01 +0000

    Add reverse_sequence method and clean unused funcs

--- vseq/utils/operations.py
+++ vseq/utils/operations.py
@@ -5,70 +5,6 @@ import torch
 
 from torch import Tensor
 
-from .shape import elevate_sample_dim
-
-
-def reduce_batch(tensor, batch_dim=0, reduction=torch.sum):
-    """Reduce the batch dimension.
-
-    Given (D*, B, D*) returns (D*, D*)
-    """
-    return reduction(tensor, axis=batch_dim)
-
-
-def reduce_samples(tensor, batch_dim=0, sample_dim=0, n_samples=None, reduction=torch.sum):
-    """Reduce the posterior or prior samples whether they are in a separate dimension or in the batch_dimension.
-
-    Given shape (B, S, D*) returns (B, D*).
-    Given shape (B * S, D*) returns (B, D*).
-
-    Typically, the D dimensions will not exist.
-    """
-    if batch_dim == sample_dim:
-        if n_samples is None:
-            raise ValueError(f"When 'batch_dim'=='sample_dim' ({batch_dim}), the number of samples must be supplied")
-        if batch_dim != 0:
-            raise ValueError(f"Cannot reduce samples for 'batch_dim' other than 0 but got '{batch_dim=}'")
-
-        tensor = elevate_sample_dim(tensor, n_samples)
-        batch_dim = batch_dim + 1
-        sample_dim = 0
-
-    return reduction(tensor, dim=sample_dim)
-
-
-def reduce_to_batch(tensor, batch_dim=0, reduction=torch.sum):
-    """Assuming that the batch dimension is the left-most dimension, reduce all others by summation.
-
-    Given shape (B, D*) returns (B,).
-    """
-    reduce_dims = list(range(tensor.ndim))
-    if not reduce_dims:
-        return tensor
-
-    reduce_dims.remove(batch_dim)
-
-    if not reduce_dims:
-        return tensor
-    return reduction(tensor, dim=reduce_dims)
-
-
-def reduce_to_latent(tensor, batch_dim=0, latent_dim=1, reduction=torch.sum):
-    """Assuming that the batch and latent dimensions are 0 and 1, respectively, reduce all others by summation.
-
-    Given shape (B, L, D*) returns (B, L).
-    """
-    reduce_dims = list(range(tensor.ndim))
-    if not reduce_dims:
-        return tensor
-
-    reduce_dims.remove(batch_dim)
-    reduce_dims.remove(latent_dim)
-
-    if not reduce_dims:
-        return tensor
-    return reduction(tensor, dim=reduce_dims)
-
 
 def log_sum_exp(tensor, axis=-1, dim=None, sum_op=torch.mean):
     """Uses the LogSumExp (LSE) as an approximation for the sum in a log-domain.
@@ -83,7 +19,7 @@ def log_sum_exp(tensor, axis=-1, dim=None, sum_op=torch.mean):
     return torch.log(sum_op(torch.exp(tensor - maximum), axis=axis, keepdim=False) + 1e-8) + maximum
 
 
-def hard_sigmoid(x, a: Union[float, Tensor] = 1/3):
+def hard_sigmoid(x, a: Union[float, Tensor] = 1 / 3):
     """Hard sigmoid function with variable slope.
 
     The variable slope is useful for annealing towards step function when estimating gradients via. a straight
@@ -96,6 +32,27 @@ def hard_sigmoid(x, a: Union[float, Tensor] = 1/3):
     return output
 
 
+@torch.jit.script
+def reverse_sequences(x, x_sl):
+    """Reverse a sequence keeping right padding untouched and in position.
+
+    Note: This method only works with right padding (not left padding or a combination).
+
+    Args:
+        x (torch.Tensor): Padded sequences to reverse (B, T, *)
+        x_sl (torch.Tensor): Sequence lengths
+
+    Returns:
+        torch.Tensor: Sequences reversed along time axis but with same padding as before
+    """
+    max_len = x_sl.max()
+    padding = (max_len - x_sl).unsqueeze(1).to(x.device)
+    reverse_ids = torch.arange(start=max_len - 1, end=-1, step=-1, device=x.device).expand(x.size(0), -1)
+    indices = reverse_ids - padding
+    indices[indices < 0] = indices[indices < 0] + max_len
+    return torch.gather(x, 1, indices)
+
+
 def sequence_mask(seq_lens: Union[list, torch.Tensor], max_len=None, dtype=torch.bool, device: torch.device = None):
     """
     Creates a binary sequence mask where all entries up to seq_lens are 1 and the remaining are 0.
@@ -106,11 +63,11 @@ def sequence_mask(seq_lens: Union[list, torch.Tensor], max_len=None, dtype=torch
         dtype (torch.dtype): The type of the mask. Default is torch.bool.
 
     Returns:
-        Tensor: The sequence mask of shape NT.
+        Tensor: The sequence mask of shape (N, T).
     """
     device = seq_lens.device if device is None else device
     if not isinstance(seq_lens, torch.Tensor):
-        seq_lens = torch.LongTensor(seq_lens) if device == torch.device('cpu') else torch.cuda.LongTensor(seq_lens)
+        seq_lens = torch.tensor(seq_lens, dtype=int, device=device)
     elif device != seq_lens.device:
         seq_lens = seq_lens.to(device)
 
@@ -143,4 +100,4 @@ def infer_device(x: Any):
     """Infer the device of any object (CPU for any non-torch object)"""
     if isinstance(x, torch.Tensor):
         return x.device
-    return torch.device('cpu')
+    return torch.device("cpu")
