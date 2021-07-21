c0087adeacf0a4726b9d239aecbd28f119c06efa
Jakob Drachmann Havtorn <jakob.havtorn@gmail.com>
2021-07-21 17:38:40 +0200

    Improvements to the Tracker (#14)
    - Dynamic width of source+progress string
    - Cutting of metrics string for narrow terminals
    - Fix bug with DDP by syncing all DDP Trackers

--- vseq/evaluation/tracker.py
+++ vseq/evaluation/tracker.py
@@ -1,10 +1,14 @@
+import collections
+import itertools
+import psutil
 import re
 
-from datetime import datetime
 from blessed import Terminal
 from collections import defaultdict
-from typing import Dict, Iterator, Union, Any, List, Optional
+from datetime import datetime
 from time import time
+from types import SimpleNamespace
+from typing import Dict, Iterable, Union, Any, List, Optional
 
 import rich
 import wandb
@@ -20,7 +24,7 @@ FORMATTING_REGEX = re.compile(FORMATTING_PATTERN)
 
 
 def length_of_formatting(string: str):
-    return sum(len(s) + 2 for s in FORMATTING_REGEX.findall(string))
+    return sum(len(s) + 2 for s in FORMATTING_REGEX.findall(string))  # plus 2 for parenthesis
 
 
 def length_without_formatting(string: str):
@@ -38,12 +42,49 @@ def rank_string(rank):
 class Tracker:
     def __init__(
         self,
-        min_indent: int = 40,
         print_every: Union[int, float] = 1.0,
         rank: Optional[int] = None,
         world_size: Optional[int] = None,
+        cpu_util_window: int = 10,
     ) -> None:
         """Tracks metrics, prints to console and logs to wandb.
+
+        Example using `.epochs()` and `.steps()`:
+        ```
+        for epoch in tracker.epochs(num_epochs):
+            for (x, x_sl), metadata in tracker.steps(train_dataloader):
+                ...
+                tracker.update(metrics)
+
+            for (x, x_sl), metadata in tracker.steps(valid_dataloader):
+                ...
+                tracker.update(metrics)
+
+            tracker.log()
+        ```
+
+        Example without `.epochs()` and `.steps()`
+        ```
+        for epoch in range(num_epochs):
+            tracker.set("train", max_steps=100)
+            for (x, x_sl), metadata in range(train_dataloader):
+                ...
+                tracker.increment_step()
+                tracker.update(metrics)
+                tracker.print()
+            tracker.unset()
+
+            tracker.set("test", max_steps=10)
+            for (x, x_sl), metadata in tracker.steps(valid_dataloader):
+                ...
+                tracker.increment_step()
+                tracker.update(metrics)
+                tracker.print()
+            tracker.unset()
+
+            tracker.log()
+            tracker.reset()
+        ```
 
         Args:
             min_indent (int): Minimum indent for dataset name. Defaults to 40.
@@ -53,10 +94,13 @@ class Tracker:
             world_size (Optional[int]): Total number of worker processes if using Distributed Data Parallel (DDP).
         """
 
-        self.min_indent = min_indent  # TODO Compute `min_indent` dynamically
         self.print_every = print_every
         self.rank = 0 if rank is None else rank
         self.world_size = world_size
+
+        # dynamic variables
+        self.max_source_str_len = 0
+        self.max_progress_str_len = 0
 
         # ddp logic
         assert (rank is None) == (world_size is None), "Must either set both `rank` and `world_size` or neither of them"
@@ -66,6 +110,7 @@ class Tracker:
         # continously updated
         self.printed_last = 0
         self.last_log_line_len = 0
+        self.cpu_utils = defaultdict(lambda: collections.deque(maxlen=cpu_util_window))
         self.source = None
         self.start_time = defaultdict(lambda: None)
         self.end_time = defaultdict(lambda: None)
@@ -75,7 +120,6 @@ class Tracker:
 
         self.metrics = defaultdict(dict)  # dict(source=dict(metric.name=metric))
         self.accumulated_metrics = defaultdict(lambda: defaultdict(list))  # dict(source=dict(metric.name=list(metric)))
-        self.accumulated_output = defaultdict(list)  # dict(key=list(value))
 
     @property
     def values(self) -> Dict[str, Dict[str, float]]:
@@ -117,20 +161,40 @@ class Tracker:
             for source in best_metrics.keys()
         }
 
-    def steps(self, loader: Union[str, DataLoader]):
-        self.set(loader)
-        for batch in loader:
+    def steps(self, steppable: Union[Iterable, DataLoader], source: Optional[str] = None):
+        if source is None and not isinstance(steppable, DataLoader):
+            raise ValueError("Must call .steps() on a DataLoader if source is None")
+
+        source = source if source is not None else steppable
+
+        self.set(source)
+
+        iterator = iter(steppable)
+
+        if hasattr(iterator, "_workers"):
+            workers = [psutil.Process(w.pid) for w in iterator._workers]
+        else:
+            workers = None
+
+        for batch in iterator:
             yield batch
+            self.increment_step()
             if self.do_print():
-                self.print()
+                self.print(workers=workers)
+
         self.unset()
 
-    def epochs(self, N) -> Iterator[int]:
+    def increment_step(self):
+        """Increment the internal step counter `self.step[self.source]`"""
+        self.step[self.source] += 1
+
+    def epochs(self, N) -> Iterable[int]:
         """Yields the epoch index while printing epoch number and epoch delimiter."""
         for epoch in range(1, N + 1):
             self.epoch = epoch
 
             if self.rank == 0:
+                # print epoch and timestamp
                 s = f"\n[bold bright_white]Epoch {epoch}:[/bold bright_white] "
                 s += "[grey30]" + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "[/]"
                 rich.print(s, flush=True)
@@ -138,26 +202,39 @@ class Tracker:
             yield epoch
 
             if self.rank == 0:
+                if self.is_ddp:
+                    # print summary of gathered and reduced metrics
+                    rich.print(
+                        f"[bold bright_white]Summary:[/bold bright_white] {' ' * (self.last_log_line_len - 18)}\n"
+                    )
+                    for source in self.best_values.keys():
+                        self.print(source=source)
+                        print(flush=True)
+
                 print("-" * (self.last_log_line_len or 50), flush=True)
+
+            self.reset()
 
     def __call__(self, loader: Union[str, DataLoader]):
         """Shortcut applicable to the standard case."""
         return self.steps(loader)
 
-    def set(self, source: Union[str, DataLoader]):
+    def set(self, source: Union[str, DataLoader], max_steps: int = None):
         """Set source name, start time and maximum number of steps if available."""
         if isinstance(source, DataLoader):
             self.source = source.dataset.source
-            self.max_steps[self.source] = len(source)
+            self.max_steps[self.source] = len(source) if max_steps is None else max_steps
         else:
             self.source = source
-            self.max_steps[self.source] = None
+            self.max_steps[self.source] = max_steps
 
         self.start_time[self.source] = time()
 
         if self.is_ddp and self.rank == 0:
             for rank in reversed(range(self.world_size)):
-                rich.print(rank_string(rank) + " " + source_string(self.source) + " " * self.last_log_line_len, flush=True)
+                rich.print(
+                    rank_string(rank) + " " + source_string(self.source) + " " * self.last_log_line_len, flush=True
+                )
             rich.print(f"Running DDP with world_size={self.world_size}", flush=True, end="\r")
 
         if self.is_ddp:
@@ -166,8 +243,9 @@ class Tracker:
     def unset(self):
         """Resets print timer and prints final line, unsets `source` and accumulates metrics."""
         self.print(end="\n")  # print line for last iteration regardless of `do_print()`
+
         if self.is_ddp:
-            distributed.barrier()
+            self.ddp_gather_and_reduce(self.source)
 
             # reset terminal to bottom of terminal window
             # NOTE This is due to risk of race condition not resetting this properly.
@@ -182,7 +260,7 @@ class Tracker:
 
         self.source = None
         self.printed_last = 0
-        self.accumulated_output = defaultdict(list)
+        self.cpu_utils[self.rank] = collections.deque(maxlen=10)
 
     def reset(self):
         """Reset all per-source attributes"""
@@ -205,7 +283,7 @@ class Tracker:
 
         return do_print
 
-    def print(self, end="\r", source: Optional[str] = None):
+    def print(self, end="\r", source: Optional[str] = None, workers: list = None):
         """Print the current progress and metric values."""
         source = self.source if source is None else source
 
@@ -215,53 +293,73 @@ class Tracker:
         else:
             steps_frac = f"{self.step[source]}/-"
 
+        ps = f"{steps_frac}"
         if self.start_time[source] is None:
             duration = "-"
-            steps_per_s = "-"
+            s_per_step = "-"
         else:
             duration = time() - self.start_time[source]
-            steps_per_s = self.step[source] / duration
-            steps_per_s = f"{round(steps_per_s, 3):.2f}Hz"
+            s_per_step = duration / self.step[source] * 1000
+            s_per_step = f"{int(s_per_step):d}ms"
             mins = int(duration // 60)
             secs = int(duration % 60)
             duration = f"{mins:d}m {secs:d}s"
 
-        ps = f"{steps_frac} [bright_white not bold]({duration}, {steps_per_s})[/]"  # +26 format
+        if workers is not None:
+            cpu = int(round(sum([p.cpu_percent(interval=0.0) for p in workers]), 0))
+            self.cpu_utils[self.rank].append(cpu)
+        if len(self.cpu_utils[self.rank]):
+            cpu = sum(self.cpu_utils[self.rank]) / len(self.cpu_utils[self.rank])
+            cpu = f"{cpu:.0f}%"
+        else:
+            cpu = "-"
+
+        ps = f"{steps_frac} [bright_white not bold]({duration}, {s_per_step}, {cpu})[/]"  # +26 format
 
         # source string
         ss = source_string(source)
 
-        # metrics string
-        sep = " [magenta]|[/]"  # +19 format pr metric
-        ms = "".join([f"{sep} {metric.name} = {metric.str_value}" for metric in self.metrics[source].values()]) + sep
+        # update dynamic string lengths
+        self.max_source_str_len = max(self.max_source_str_len, len(ss))
+        self.max_progress_str_len = max(self.max_progress_str_len, len(ps))
 
-        # full log string
-        sp = f"{ss} - {ps}"
-        s = f"{sp:<{self.min_indent + length_of_formatting(sp)}s}{ms}"
+        # source progress string
+        sp = f"{ss:<{self.max_source_str_len}} - {ps:<{self.max_progress_str_len}}"
+        if self.is_ddp:
+            sp = rank_string(self.rank) + " " + sp
+            end = "\r" if self.rank == 0 else "\n"
+
+        # metrics string
+        sep = " [magenta]|[/] "  # " | "
+        metrics = [f"{name} = {met.str_value}" for name, met in self.metrics[source].items() if met.log_to_console]
+        metrics_len = [3 + len(m) for m in metrics]  # 3 is length of sep without formatting
+        metrics_len[0] += 3  # add sep left of first metric
+        metrics_len[-1] -= 1  # remove space right of sep right of last metric
+        metrics_cumlen = list(itertools.accumulate(metrics_len))
+        max_metrics_str_len = self.terminal.width - length_without_formatting(sp)
+        if metrics_cumlen[-1] > max_metrics_str_len:
+            idx = next(i for i, v in enumerate(metrics_cumlen) if v > max_metrics_str_len - 3)  # last before too long
+            metrics = metrics[:idx] + ["..."]
+        ms = sep + sep.join(metrics)
+
+        # final string
+        s = f"{sp:<}{ms}"
+
+        self.last_log_line_len = length_without_formatting(s)
+        s = s + " " * 5  # add some whitespace to overwrite any lingering characters
 
         if self.is_ddp:
             # TODO Instead of rich, print with regular print and add colors manually.
             #      The current implementation has a race condition on placing the cursor
             #      and printing the line with rich. This is merged to one print call without rich.
             # print(self.terminal.move_y(self.terminal.height - self.rank - 2) + s, end='\r')#, flush=True)
-            end = "\r" if self.rank == 0 else "\n"
-            s = rank_string(self.rank) + " " + s
-            s_len = length_without_formatting(s)
-            s = s + " " * (self.terminal.width - s_len)
             with self.terminal.location(0, self.terminal.height - 2 - self.rank):
                 rich.print(s, end=end, flush=True)
         else:
-            s_len = length_without_formatting(s)
-            s = s + " " * (self.terminal.width - s_len)
             rich.print(s, end=end, flush=True)
-
-        self.last_log_line_len = s_len
 
     def log(self, **extra_log_data: Dict[str, Any]):
         """Log all tracked metrics to experiment tracking framework and reset `metrics`."""
-        if self.is_ddp:
-            self.ddp_gather_and_reduce()
-
         # add best and tracker metrics and any `extra_log_data`
         values = self.values
         values.update(extra_log_data)
@@ -272,47 +370,45 @@ class Tracker:
         if self.rank == 0:
             wandb.log(values)
 
-        self.reset()
-
     def update(self, metrics: List[Metric], source: Optional[str] = None):
         """Update all metrics tracked on `source` with the given `metrics` and add any not currently tracked"""
         names = [metric.name for metric in metrics]
         assert len(names) == len(set(names)), "Metrics must have unique names"
         source = self.source if source is None else source
 
-        self.step[self.source] += 1
+        if self.start_time[source] is None:
+            self.start_time[source] = time()
+
         for metric in metrics:
             if metric.name in self.metrics[source]:
                 self.metrics[source][metric.name].update(metric)
             else:
                 self.metrics[source][metric.name] = metric.copy()
 
-    def accumulate(self, **kwargs: Dict[Any, Any]):
-        """Accumulate some outputs of interest. Gets reset on every call to `reset()` (e.g. epoch)"""
-        for k, v in kwargs.items():
-            self.accumulated_output[k].append(v)
+    def ddp_gather_and_reduce(self, source):
+        """Share metrics across all `Tracker` objects, reduce them in `rank==self.rank` and update the Tracker"""
+        # gather various variables across processes
+        namespace_to_gather = SimpleNamespace(
+            rank=self.rank,
+            metrics=self.metrics[source],
+            max_source_str_len=self.max_source_str_len,
+            max_progress_str_len=self.max_progress_str_len,
+        )
+        gathered_namespaces = [None] * self.world_size
+        distributed.all_gather_object(gathered_namespaces, namespace_to_gather)
 
-    def ddp_gather_and_reduce(self):
-        """Share metrics across all `Tracker` objects, reduce them in `rank==0` and update that Tracker"""
-        # gather metric objects across processes
-        metrics_per_rank = [None] * self.world_size
-        distributed.all_gather_object(metrics_per_rank, (self.rank, self.metrics))
+        # filter gathered metrics
+        gathered_metrics = [ns.metrics for ns in gathered_namespaces if ns.rank != self.rank]
 
-        if self.rank == 0:
-            # reduce gathered metrics
-            gathered_metrics = [metrics for (rank, metrics) in metrics_per_rank if rank != 0]
-            for source in self.best_values.keys():
-                # update metrics
-                for metrics in gathered_metrics:
-                    m = list(metrics[source].values())
-                    self.update(m, source=source)
+        # update metrics
+        for metrics in gathered_metrics:
+            m = list(metrics.values())
+            self.update(m, source=source)
 
-                # update steps to match total steps taken
-                self.step[source] = self.step[source] * (len(gathered_metrics) + 1)
-                self.max_steps[source] = self.max_steps[source] * (len(gathered_metrics) + 1)
+        # sync log line width
+        self.max_source_str_len = max([ns.max_source_str_len for ns in gathered_namespaces])
+        self.max_progress_str_len = max([ns.max_progress_str_len for ns in gathered_namespaces])
 
-            # print summary of gathered and redued metrics
-            rich.print(f"[bold bright_white]Summary:[/bold bright_white] {' ' * (self.last_log_line_len - 18)}\n")
-            for source in self.best_values.keys():
-                self.print(source=source)
-                print(flush=True)
+        # update steps to match total steps taken
+        self.step[source] = self.step[source] * self.world_size
+        self.max_steps[source] = self.max_steps[source] * self.world_size
