Implement stronger prior and global latent, fix KL in bits
- Stronger prior by more GRU cells
- Global latent inferred via existing ladder and recurrent attention
- Fix KL conversion to bpt bug (division by log(2))

experiments/experiment_cwvae_audio.py
@@ -31,8 +31,10 @@ parser.add_argument("--length_sampler", default=True, type=str2bool, help="use l
 parser.add_argument("--lr", default=3e-4, type=float, help="base learning rate")
 parser.add_argument("--hidden_size", default=512, type=int, nargs="+", help="dimensionality of hidden state in CWVAE")
 parser.add_argument("--latent_size", default=128, type=int, nargs="+", help="dimensionality of latent state in CWVAE")
+parser.add_argument("--global_size", default=None, type=int, help="dimensionality of global latent state in CWVAE")
 parser.add_argument("--time_factors", default=[200, 800, 3200], type=int, nargs="+", help="temporal abstraction factor")
 parser.add_argument("--num_level_layers", default=8, type=int, help="dense layers for embedding per level")
+parser.add_argument("--num_rssm_gru_cells", default=3, type=int, help="number of stacked GRU cells in an RSSM cell")
 parser.add_argument("--input_coding", default="mu_law", type=str, choices=["mu_law", "frames"], help="input encoding")
 parser.add_argument("--num_bits", default=8, type=int, help="number of bits for DML and input")
 parser.add_argument("--num_mix", default=10, type=int, help="number of logistic mixture components")
@@ -92,11 +94,13 @@ rich.print(vars(args))
 model = vseq.models.CWVAEAudioTasNet(
     z_size=args.latent_size,
     h_size=args.hidden_size,
+    g_size=args.global_size,
     time_factors=args.time_factors,
     num_level_layers=args.num_level_layers,
     num_mix=args.num_mix,
     num_bins=2 ** args.num_bits,
-    residual_posterior=args.residual_posterior
+    residual_posterior=args.residual_posterior,
+    num_rssm_gru_cells=args.num_rssm_gru_cells,
 )
 # model = vseq.models.CWVAEAudioDense(
 # # model = vseq.models.CWVAEAudioConv1D(
@@ -184,9 +188,8 @@ else:
     )
 
 
-
 print(model)
-model.summary(input_size=(4, model.overall_stride), x_sl=torch.tensor([model.overall_stride]), device="cpu")
+model.summary(input_size=(4, 10 * model.overall_stride), x_sl=torch.tensor([10 * model.overall_stride] * 4), device="cpu")
 model = model.to(device)
 wandb.watch(model, log="all", log_freq=len(train_loader))
 

vseq/models/clockwork_vae/cwvae.py
@@ -20,6 +20,7 @@ from vseq.evaluation.metrics import (
 )
 from vseq.models.base_model import BaseModel
 from vseq.modules.distributions import DiscretizedLaplaceMixtureDense, DiscretizedLogisticMixtureDense, GaussianDense
+from vseq.modules.rssm import RSSMCell, RSSMCellSingleGRU
 from vseq.utils.variational import discount_free_nats, kl_divergence_gaussian
 from vseq.utils.operations import sequence_mask
 
@@ -27,101 +28,12 @@ from .dense_coders import DenseAudioEncoder, DenseAudioDecoder
 from .tasnet_coder import TasNetCoder, TasNetDecoder, TasNetEncoder
 from .cpc_coders import CPCDecoder, CPCEncoder
 from .conv_coders import AudioEncoderConv1d, AudioDecoderConv1d, ContextDecoderConv1d
+from .global_coders import GlobalCoder
 
 
 def get_exponential_time_factors(abs_factor, num_levels):
     """Return exponentially increasing temporal abstraction factors with base `abs_factor`"""
     return [abs_factor ** l for l in range(num_levels)]
-
-
-class RSSMCell(torch.jit.ScriptModule):
-    def __init__(self, z_dim: int, h_dim: int, e_dim: int, c_dim: int, residual_posterior: bool = False):
-        """Recurrent State Space Model cell
-
-        Args:
-            z_dim (int): Dimensionality stochastic state space.
-            h_dim (int): Dimensionality of deterministic state space.
-            e_dim (int): Dimensionalit of input embedding space.
-            c_dim (int): Dimensionality of "context".
-        """
-        super().__init__()
-
-        self.z_dim = z_dim
-        self.h_dim = h_dim
-        self.e_dim = e_dim
-        self.c_dim = c_dim
-        self.residual_posterior = residual_posterior
-
-        self.gru_in = nn.Sequential(nn.Linear(z_dim + c_dim, h_dim), nn.ReLU())
-        self.gru_cell = nn.GRUCell(h_dim, h_dim)
-
-        self.prior = nn.Sequential(
-            nn.Linear(h_dim, h_dim),
-            nn.ReLU(),
-            GaussianDense(h_dim, z_dim),
-        )
-
-        self.posterior = nn.Sequential(
-            nn.Linear(h_dim + e_dim, h_dim),
-            nn.ReLU(),
-            nn.Linear(h_dim, h_dim),
-            nn.ReLU(),
-            GaussianDense(h_dim, z_dim),
-        )
-
-    def get_initial_state(self, batch_size: int, device: str = None):
-        device = device if device is not None else self.prior[0].weight.device
-        return (torch.zeros(batch_size, self.z_dim, device=device), torch.zeros(batch_size, self.h_dim, device=device))
-
-    def get_empty_context(self, batch_size: int, device: str = None):
-        device = device if device is not None else self.prior[0].weight.device
-        return torch.empty(batch_size, 0, device=device)
-
-    def forward(
-        self,
-        enc_inputs: torch.Tensor,
-        state: Tuple[torch.Tensor, torch.Tensor],
-        context: torch.Tensor,
-        temperature: float = 1.0,
-        use_mode: bool = False,
-    ):
-        z, h = state
-
-        gru_in = self.gru_in(torch.cat([z, context], dim=-1))
-        h_new = self.gru_cell(gru_in, h)
-
-        prior_mu, prior_sd = self.prior(h_new)
-
-        enc_mu, enc_sd = self.posterior(torch.cat([h_new, enc_inputs], dim=-1))
-
-        if self.residual_posterior:
-            enc_mu = enc_mu + prior_mu
-
-        z_new = self.posterior[-1].rsample((enc_mu, temperature * enc_sd)) if not use_mode else enc_mu
-
-        distributions = SimpleNamespace(z=z_new, enc_mu=enc_mu, enc_sd=enc_sd, prior_mu=prior_mu, prior_sd=prior_sd)
-
-        return (z_new, h_new), distributions
-
-    @torch.jit.export
-    def generate(
-        self,
-        state: Tuple[torch.Tensor, torch.Tensor],
-        context: torch.Tensor,
-        temperature: float = 1.0,
-        use_mode: bool = False,
-    ):
-        z, h = state
-
-        gru_in = self.gru_in(torch.cat([z, context], dim=-1))
-        h_new = self.gru_cell(gru_in, h)
-
-        prior_mu, prior_sd = self.prior(h_new)
-        z_new = self.prior[-1].rsample((prior_mu, temperature * prior_sd)) if not use_mode else prior_mu
-
-        distributions = SimpleNamespace(z=z_new, prior_mu=prior_mu, prior_sd=prior_sd)
-
-        return (z_new, h_new), distributions
 
 
 class CWVAE(nn.Module):
@@ -133,10 +45,27 @@ class CWVAE(nn.Module):
         encoder: nn.Module,
         decoder: nn.Module,
         likelihood: nn.Module,
+        g_size: Optional[int] = 0,
         context_decoder: Optional[nn.ModuleList] = None,
         residual_posterior: bool = False,
+        num_rssm_gru_cells: int = 1,
         with_resets: bool = False,
     ):
+        """Clockwork VAE like latent variable model
+
+        Args:
+            z_size (Union[int, List[int]]): Size(s) of the temporal latent variables.
+            h_size (Union[int, List[int]]): Size(s) of the temporal deterministic variables.
+            g_size (Optional[int]): Size of the optional global latent variable. Defaults to 0.
+            time_factors (List[int]): Overall strides per layer.
+            encoder (nn.Module): Transformation used to infer deterministic representations of input.
+            decoder (nn.Module): Transformation used to decode context output by the final layer.
+            likelihood (nn.Module): Transformation used to evaluate the likelihood (log_prob) of the reconstruction.
+            context_decoder (Optional[nn.ModuleList], optional): Decoder for between-layer contexts. Defaults to None.
+            residual_posterior (bool, optional): If True, compute mu_q = mu_q' + mu_p. Defaults to False.
+            num_rssm_gru_cells (int, optional): Number of stacked GRU cells used per RSSM cell. Defaults to 1.
+            with_resets (bool, optional): Rset state whenever layer above ticks (never reset top). Defaults to False.
+        """
         super().__init__()
 
         assert isinstance(time_factors, list)
@@ -148,6 +77,8 @@ class CWVAE(nn.Module):
         self.num_levels = len(time_factors)
         self.time_factors = time_factors
         self.residual_posterior = residual_posterior
+        self.num_rssm_gru_cells = num_rssm_gru_cells
+        self.g_size = g_size
         self.with_resets = with_resets
 
         self.strides = [self.time_factors[0]]
@@ -156,7 +87,8 @@ class CWVAE(nn.Module):
         self.z_size = [z_size] * self.num_levels if isinstance(z_size, int) else z_size
         self.h_size = [h_size] * self.num_levels if isinstance(h_size, int) else h_size
         if context_decoder is None:
-            self.c_size = [z_dim + h_dim for z_dim, h_dim in zip(self.z_size[1:], self.h_size[1:])] + [0]
+            self.c_size = [z_dim + h_dim * num_rssm_gru_cells + g_size for z_dim, h_dim in zip(self.z_size[1:], self.h_size[1:])]
+            self.c_size += [0 + g_size]  # top context is empty or global latent
         else:
             # When using context decoder that changes num_channels to that of layer below
             self.c_size = [h_dim for z_dim, h_dim in zip(self.z_size[:-1], self.h_size[:-1])] + [0]
@@ -168,25 +100,29 @@ class CWVAE(nn.Module):
         cells = []
         for h_dim, z_dim, c_dim, e_dim in zip(self.h_size, self.z_size, self.c_size, encoder.e_size):
             cells.append(
-                RSSMCell(
+                RSSMCell(  # RSSMCellSingleGRU
                     h_dim=h_dim,
                     z_dim=z_dim,
                     e_dim=e_dim,
                     c_dim=c_dim,
                     residual_posterior=residual_posterior,
+                    n_gru_cells=num_rssm_gru_cells,
                 )
             )
         self.cells = nn.ModuleList(cells)
 
+        if g_size > 0:
+            self.global_coder = GlobalCoder(g_size, encoder.e_size)
+
         self.receptive_field = self.encoder.receptive_field
         self.overall_stride = self.encoder.overall_stride
 
-    def build_metrics(self, loss, elbo, log_prob, kld, klds, enc_mus, prior_mus, x_sl, seq_mask, beta, free_nats):
-        kld_metrics_nats = [
-            KLMetric(klds[l], name=f"kl_{l} (nats)", log_to_console=False) for l in range(self.num_levels)
+    def build_metrics(self, loss, elbo, log_prob, kld, kld_l, kld_g, enc_mus, prior_mus, x_sl, seq_mask, beta, free_nats):
+        kld_layers_metrics_nats = [
+            KLMetric(kld_l[l], name=f"kl_{l} (nats)", log_to_console=False) for l in range(self.num_levels)
         ]
-        kld_metrics_bpd = [
-            KLMetric(klds[l], name=f"kl_{l} (bpt)", reduce_by=(x_sl / (math.log(2) * self.time_factors[l])))
+        kld_layers_metrics_bpd = [
+            KLMetric(kld_l[l] / math.log(2), name=f"kl_{l} (bpt)", reduce_by=(x_sl / self.time_factors[l]))
             for l in range(self.num_levels)
         ]
         latent_activity_metrics_percent = [
@@ -199,7 +135,6 @@ class CWVAE(nn.Module):
             )
             for l in range(self.num_levels)
         ]
-        #
         latent_activity_metrics_variance = [
             LatentActivityMetric(
                 enc_mus[l][:, : x_sl.min()],
@@ -212,50 +147,64 @@ class CWVAE(nn.Module):
 
         metrics = [
             LossMetric(loss, weight_by=elbo.numel()),
+            LatestMeanMetric(elbo / math.log(2), name="last elbo (bpt)", reduce_by=-x_sl),
             LLMetric(elbo, name="elbo (nats)"),
             BitsPerDimMetric(elbo, name="elbo (bpt)", reduce_by=x_sl),
             LLMetric(log_prob, name="rec (nats)", log_to_console=False),
             BitsPerDimMetric(log_prob, name="rec (bpt)", reduce_by=x_sl),
             KLMetric(kld, name="kl (nats)"),
-            KLMetric(kld, name="kl (bpt)", reduce_by=x_sl / (math.log(2) * self.time_factors[0])),
-            *kld_metrics_nats,
-            *kld_metrics_bpd,
+            KLMetric(kld / math.log(2), name="kl (bpt)", reduce_by=x_sl / self.time_factors[0]),
+            *kld_layers_metrics_nats,
+            *kld_layers_metrics_bpd,
             *latent_activity_metrics_percent,
             *latent_activity_metrics_variance,
             LatestMeanMetric(beta, name="beta"),
             LatestMeanMetric(free_nats, name="free_nats"),
         ]
+
+        if kld_g is not None:
+            metrics.extend([
+                KLMetric(kld_g, name="kl_g (nats)"),
+                KLMetric(kld_g / math.log(2), name="kl_g (bits)"),
+            ])
+
         return metrics
 
     def compute_elbo(
         self,
         y: TensorType["B", "T", "D"],
+        seq_mask: TensorType["B", "T", int],
+        x_sl: TensorType["B", int],
         parameters: TensorType["B", "T", "D"],
         kld_layerwise: List[TensorType["B", "T", "latent_size"]],
-        x_sl: TensorType["B", int],
+        kld_global: Optional[TensorType["B", "g_size"]] = None,
         beta: float = 1,
         free_nats: float = 0,
     ):
         """Return reduced loss for batch and non-reduced ELBO, log p(x|z) and KL-divergence"""
-        seq_mask = sequence_mask(x_sl, max_len=y.shape[1], dtype=float, device=y.device)
-
         log_prob_twise = self.likelihood.log_prob(y, parameters) * seq_mask
         log_prob = log_prob_twise.view(y.size(0), -1).sum(1)  # (B,)
 
-        klds, klds_fn = [], []
+        kld_l, klds_fn = [], []
         seq_mask = seq_mask.unsqueeze(-1)  # Broadcast to latent dimension
         for l in range(self.num_levels):
-            mask = seq_mask[:, :: self.time_factors[l]]  # Faster than creating new mask
-            klds.append((kld_layerwise[l] * mask).sum((1, 2)))  # (B,)
+            mask = seq_mask[:, :: self.time_factors[l]]  # Mask for subsampled layers  (faster than creating new mask)
+            kld_l.append((kld_layerwise[l] * mask).sum((1, 2)))  # (B,)
             klds_fn.append((discount_free_nats(kld_layerwise[l], free_nats, shared_dims=-1) * mask).sum((1, 2)))  # (B,)
 
-        kld, kld_fn = sum(klds), sum(klds_fn)
+        kld, kld_fn = sum(kld_l), sum(klds_fn)
+
+        if kld_global is not None:
+            kld_fn_global = discount_free_nats(kld_global, free_nats, shared_dims=-1).sum(-1)
+            kld_global = kld_global.sum(-1)
+
+            kld, kld_fn = kld + kld_global, kld_fn + kld_fn_global
 
         elbo = log_prob - kld  # (B,)
 
         loss = -(log_prob - beta * kld_fn).sum() / x_sl.sum()  # (1,)
 
-        return loss, elbo, log_prob, kld, klds, seq_mask
+        return loss, elbo, log_prob, kld, kld_l, kld_global
 
     def forward(
         self,
@@ -270,16 +219,26 @@ class CWVAE(nn.Module):
         if y is None:
             y = x.clone().detach()
 
+        seq_mask = sequence_mask(x_sl, max_len=y.shape[1], dtype=bool, device=y.device)
+
         # compute encodings
-        encodings = self.encoder(x)
-        encodings = [enc.unbind(1) for enc in encodings]
+        encodings_list = self.encoder(x)
+        encodings = [enc.unbind(1) for enc in encodings_list]  # unbind time dimension (List[Tuple[Tensor]])
+
+        # global latent
+        if self.g_size > 0:
+            g, g_distributions = self.global_coder(encodings_list, seq_mask, self.time_factors)
+            kld_g = kl_divergence_gaussian(g_distributions.enc_mu, g_distributions.enc_sd, g_distributions.prior_mu, g_distributions.prior_sd)
+        else:
+            g = torch.empty(x.size(0), 0, device=y.device)
+            kld_g = None
 
         # initial context for top layer
-        context_l = [self.cells[0].get_empty_context(batch_size=x.size(0))] * len(encodings[-1])
+        context_l = [g] * len(encodings[-1])
 
         # initial RSSM state (z, h)
         states = [cell.get_initial_state(batch_size=x.size(0)) for cell in self.cells] if state0 is None else state0
-        klds = [[] for _ in range(self.num_levels)]
+        kld_l = [[] for _ in range(self.num_levels)]
         latents = [[] for _ in range(self.num_levels)]
         enc_mus = [[] for _ in range(self.num_levels)]
         prior_mus = [[] for _ in range(self.num_levels)]
@@ -299,7 +258,7 @@ class CWVAE(nn.Module):
                 all_distributions.append(distributions)
 
             # update context_l for below layer as cat(z_l, h_l)
-            context_l = [torch.cat(all_states[t], dim=-1) for t in range(T_l)]
+            context_l = [torch.cat([*all_states[t], g], dim=-1) for t in range(T_l)]
             if l >= 1:
                 # TODO context decoder could be a single decoder with as many layers as encoder (or optionally fewer)
                 # if self.decoder[l - 1] is None:
@@ -330,8 +289,7 @@ class CWVAE(nn.Module):
             enc_mus[l] = enc_mu
             prior_mus[l] = prior_mu
 
-            kld_l = kl_divergence_gaussian(enc_mu, enc_sd, prior_mu, prior_sd)
-            klds[l] = kld_l
+            kld_l[l] = kl_divergence_gaussian(enc_mu, enc_sd, prior_mu, prior_sd)
 
         context_l = torch.stack(context_l, dim=1)
         dec = self.decoder(context_l)
@@ -340,10 +298,10 @@ class CWVAE(nn.Module):
 
         reconstruction = self.likelihood.sample(parameters)
 
-        loss, elbo, log_prob, kld, klds, seq_mask = self.compute_elbo(y, parameters, klds, x_sl, beta, free_nats)
+        loss, elbo, log_prob, kld, kld_l, kld_g = self.compute_elbo(y, seq_mask, x_sl, parameters, kld_l, kld_g, beta, free_nats)
 
         metrics = self.build_metrics(
-            loss, elbo, log_prob, kld, klds, enc_mus, prior_mus, x_sl, seq_mask, beta, free_nats
+            loss, elbo, log_prob, kld, kld_l, kld_g, enc_mus, prior_mus, x_sl, seq_mask, beta, free_nats
         )
 
         outputs = SimpleNamespace(
@@ -367,6 +325,7 @@ class CWVAE(nn.Module):
         max_timesteps: int = 100,
         use_mode: bool = False,
         use_mode_latents: bool = False,
+        use_mode_global: bool = False,
         use_mode_observations: bool = False,
         temperature: float = 1,
         x: Optional[TensorType["B", "T", "D"]] = None,
@@ -375,15 +334,19 @@ class CWVAE(nn.Module):
         # initial RSSM state (z, h)
         states = [cell.get_initial_state(batch_size=n_samples) for cell in self.cells] if state0 is None else state0
 
+        # global latent
+        if self.g_size > 0:
+            g, g_distributions = self.global_coder.generate(batch_size=n_samples, temperature=temperature, use_mode=use_mode_global)
+        else:
+            g = self.cells[0].get_empty_context(batch_size=n_samples)
+
         if x is not None:
             raise NotImplementedError("Conditional generation is not implemented")
             # TODO Run a forward pass over x to get conditional initial state0 (assert state0 is None)
             #      Construct context_l as concatenation of state0 of layer above (empty for top layer)
         else:
             # initial context_l for top layer
-            context_l = [self.cells[0].get_empty_context(batch_size=n_samples)] * (
-                max_timesteps // self.time_factors[-1]
-            )
+            context_l = [g] * (max_timesteps // self.time_factors[-1])
 
         for l in range(self.num_levels - 1, -1, -1):
             all_states = []
@@ -403,7 +366,7 @@ class CWVAE(nn.Module):
                 all_distributions.append(distributions)
 
             # update context_l for below layer as cat(z_l, h_l)
-            context_l = [torch.cat(all_states[t], dim=-1) for t in range(T_l)]
+            context_l = [torch.cat([*all_states[t], g], dim=-1) for t in range(T_l)]
             if l >= 1:
                 if self.context_decoder is None:
                     # repeat context to temporal resolution of below layer
@@ -726,10 +689,12 @@ class CWVAEAudioTasNet(BaseModel):
         self,
         z_size: Union[int, List[int]] = 64,
         h_size: Union[int, List[int]] = 128,
+        g_size: Optional[int] = 0,
         time_factors: Union[int, List[int]] = [64, 512, 4096],  # Rename strides
         dilations: Union[int, List[int]] = 1,
         residual_posterior: bool = False,
         num_level_layers: int = 8,
+        num_rssm_gru_cells: int = 1,
         num_mix: int = 10,
         num_bins: int = 256,
         norm_type: str = "ChannelwiseLayerNorm",
@@ -740,10 +705,12 @@ class CWVAEAudioTasNet(BaseModel):
 
         self.z_size = z_size
         self.h_size = h_size
+        self.g_size = g_size
         self.time_factors = time_factors
         self.dilations = dilations
         self.residual_posterior = residual_posterior
         self.num_level_layers = num_level_layers
+        self.num_rssm_gru_cells = num_rssm_gru_cells
         self.num_mix = num_mix
         self.num_bins = num_bins
         self.norm_type = norm_type
@@ -752,7 +719,7 @@ class CWVAEAudioTasNet(BaseModel):
 
         bot_z_size = z_size if isinstance(z_size, int) else z_size[0]
         bot_h_size = h_size if isinstance(h_size, int) else h_size[0]
-        bot_c_size = bot_z_size + bot_h_size
+        bot_c_size = bot_z_size + bot_h_size * num_rssm_gru_cells + g_size
 
         assert all(h_size[0] == hs for hs in h_size)
         h_size = h_size[0]
@@ -813,6 +780,8 @@ class CWVAEAudioTasNet(BaseModel):
             h_size=h_size,
             time_factors=time_factors,
             residual_posterior=residual_posterior,
+            num_rssm_gru_cells=num_rssm_gru_cells,
+            g_size=g_size,
         )
         self.receptive_field = self.cwvae.receptive_field
         self.overall_stride = self.cwvae.overall_stride